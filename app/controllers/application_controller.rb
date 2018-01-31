c79e4b30a183ce90fc492c0f21c13d6b37e5af1b
Paul <paul.ryan.iii@gmail.com>
2018-01-30 18:00:51 -0800

    Start user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logout
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logout
+    session[:session_token] = nil
+    @current_user.reset_session_token
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to session_url unless logged_in?
+  end
 end
