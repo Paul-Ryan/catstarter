c87003252b195d494861908c21db35ba507c2690
Paul <paul.ryan.iii@gmail.com>
2018-04-10 21:52:23 -0700

    Imporove styling of homepage

--- frontend/components/splashpage/featured.jsx
+++ frontend/components/splashpage/featured.jsx
@@ -15,8 +15,10 @@ function Featured(props) {
       <Link to={`projects/${featured.id}`}>
         <h2>FEATURED PROJECT</h2>
         <div className="featured-projects" style={backgroundImg}>
-          <p className="featured-title">{featured.title}</p>
-          <p className="featured-author">By {featured.author.username}</p>
+          <div className="featured-text">
+            <p className="featured-title">{featured.title}</p>
+            <p className="featured-author">By {featured.author.username}</p>
+          </div>
         </div>
       </Link>
     </div>
